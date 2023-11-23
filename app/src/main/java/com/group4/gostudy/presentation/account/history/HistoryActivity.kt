e34754408f892481b906b3117096394f86070459
yudiatmoko <yudiatmokoilham@gmail.com>
2023-11-23 22:07:57 +0700

    Create UI History Page

--- app/src/main/java/com/group4/gostudy/presentation/account/history/HistoryActivity.kt
+++ app/src/main/java/com/group4/gostudy/presentation/account/history/HistoryActivity.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.group4.gostudy.databinding.ActivityHistoryBinding
+import com.group4.gostudy.model.HistoryProvider
 
 class HistoryActivity : AppCompatActivity() {
 
@@ -12,11 +15,26 @@ class HistoryActivity : AppCompatActivity() {
         ActivityHistoryBinding.inflate(layoutInflater)
     }
 
+    private val historyAdapter: HistoryListAdapter by lazy {
+        HistoryListAdapter {}
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         setClickListener()
+        setHistoryRV()
+    }
+
+    private fun setHistoryRV() {
+        binding.rvHistoryList.apply {
+            layoutManager = LinearLayoutManager(this@HistoryActivity, RecyclerView.VERTICAL, false)
+            adapter = historyAdapter
+            historyAdapter.setData(
+                HistoryProvider.getDummyData()
+            )
+        }
     }
 
     private fun setClickListener() {
