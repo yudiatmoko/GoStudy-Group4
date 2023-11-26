ac641e85e251da451b4af0279533dfe1aaa2838e
rizkyyilman <nuugrahaaa@gmail.com>
2023-11-27 03:16:34 +0700

    adding activity login, register, splashscreen

---
+++ app/src/main/java/com/group4/gostudy/presentation/splashscreen/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.group4.gostudy.presentation.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.group4.gostudy.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
