184ea7d7a0148d206f506fd92be1707581376323
yudiatmoko <yudiatmokoilham@gmail.com>
2023-11-27 02:02:06 +0700

    fixing ChangePasswordActivity bug

--- app/src/main/java/com/group4/gostudy/presentation/account/changepassword/ChangePasswordActivity.kt
+++ app/src/main/java/com/group4/gostudy/presentation/account/changepassword/ChangePasswordActivity.kt
@@ -1,13 +1,41 @@
 package com.group4.gostudy.presentation.account.changepassword
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.group4.gostudy.R
+import androidx.core.view.isVisible
+import com.group4.gostudy.databinding.ActivityChangePasswordBinding
 
-class ChangePasswordActivity :
-    AppCompatActivity() {
+class ChangePasswordActivity : AppCompatActivity() {
+
+    private val binding: ActivityChangePasswordBinding by lazy {
+        ActivityChangePasswordBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_change_password)
+        setContentView(binding.root)
+
+        setClickListener()
+        setForm()
+    }
+
+    private fun setForm() {
+        binding.layoutForm.tilOldPassword.isVisible = true
+        binding.layoutForm.tilNewPassword.isVisible = true
+        binding.layoutForm.tilConfirmNewPassword.isVisible = true
+    }
+
+    private fun setClickListener() {
+        binding.toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    companion object {
+        fun startActivity(context: Context) {
+            val intent = Intent(context, ChangePasswordActivity::class.java)
+            context.startActivity(intent)
+        }
     }
 }
