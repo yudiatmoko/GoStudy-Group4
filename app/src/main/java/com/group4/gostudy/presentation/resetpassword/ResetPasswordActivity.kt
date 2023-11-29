cd3f18af32b59279734f16e815d184281c8fcc96
rizkyyilman <nuugrahaaa@gmail.com>
2023-11-29 19:43:01 +0700

    adding ui

---
+++ app/src/main/java/com/group4/gostudy/presentation/resetpassword/ResetPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.group4.gostudy.presentation.resetpassword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.group4.gostudy.R
+
+class ResetPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password)
+    }
+}
