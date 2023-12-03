d1bbf02fc169f9487a3fa506bd03ebfe669c6d7f
Ninda Ack <ninda2752@gmail.com>
2023-12-03 19:02:50 +0700

    edit dummy data detail course

--- app/src/main/java/com/group4/gostudy/model/SectionedData.kt
+++ app/src/main/java/com/group4/gostudy/model/SectionedData.kt
@@ -35,33 +35,3 @@ object DummyDetailCourseMaterialDataSource {
         )
     }
 }
-
-object DummyDetailCourseMaterialDataSource {
-    fun getListData(): List<SectionedData> {
-        return listOf(
-            SectionedData(
-                "Chapter 1 - Pendahuluan", "60 menit", listOf(
-                    "Tujuan mengikuti kelas Design System",
-                    "Pengenalan Design System",
-                    "Contoh dalam membagun Design System"
-                )
-            ),
-            SectionedData(
-                "Chapter 2 - Memulai Desain", "120 menit", listOf(
-                    "Color Palette",
-                    "Typography",
-                    "Layout dan Grid",
-                    "Button Component"
-                )
-            ),
-            SectionedData(
-                "Chapter 3 - Workflow", "120", listOf(
-                    "Color Palette",
-                    "Typography",
-                    "Layout dan Grid",
-                    "Button Component"
-                )
-            ),
-        )
-    }
-}
