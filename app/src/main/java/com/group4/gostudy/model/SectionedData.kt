b063856db59bb2114315405318a0bcdf28c8721d
Ninda Ack <ninda2752@gmail.com>
2023-12-03 19:01:50 +0700

    edit layout and add component

--- app/src/main/java/com/group4/gostudy/model/SectionedData.kt
+++ app/src/main/java/com/group4/gostudy/model/SectionedData.kt
@@ -5,3 +5,63 @@ data class SectionedData(
     val time: String,
     val data: List<String>
 )
+
+object DummyDetailCourseMaterialDataSource {
+    fun getListData(): List<SectionedData> {
+        return listOf(
+            SectionedData(
+                "Chapter 1 - Pendahuluan", "60 menit", listOf(
+                    "Tujuan mengikuti kelas Design System",
+                    "Pengenalan Design System",
+                    "Contoh dalam membagun Design System"
+                )
+            ),
+            SectionedData(
+                "Chapter 2 - Memulai Desain", "120 menit", listOf(
+                    "Color Palette",
+                    "Typography",
+                    "Layout dan Grid",
+                    "Button Component"
+                )
+            ),
+            SectionedData(
+                "Chapter 3 - Workflow", "120", listOf(
+                    "Color Palette",
+                    "Typography",
+                    "Layout dan Grid",
+                    "Button Component"
+                )
+            ),
+        )
+    }
+}
+
+object DummyDetailCourseMaterialDataSource {
+    fun getListData(): List<SectionedData> {
+        return listOf(
+            SectionedData(
+                "Chapter 1 - Pendahuluan", "60 menit", listOf(
+                    "Tujuan mengikuti kelas Design System",
+                    "Pengenalan Design System",
+                    "Contoh dalam membagun Design System"
+                )
+            ),
+            SectionedData(
+                "Chapter 2 - Memulai Desain", "120 menit", listOf(
+                    "Color Palette",
+                    "Typography",
+                    "Layout dan Grid",
+                    "Button Component"
+                )
+            ),
+            SectionedData(
+                "Chapter 3 - Workflow", "120", listOf(
+                    "Color Palette",
+                    "Typography",
+                    "Layout dan Grid",
+                    "Button Component"
+                )
+            ),
+        )
+    }
+}
