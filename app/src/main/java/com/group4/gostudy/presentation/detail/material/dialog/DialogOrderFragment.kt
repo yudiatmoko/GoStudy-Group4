e875f659d70bcc95f7a5f6c6963cfcea17359368
Ninda Ack <ninda2752@gmail.com>
2023-12-11 23:15:43 +0700

    add viewmodel dialog order

--- app/src/main/java/com/group4/gostudy/presentation/detail/material/dialog/DialogOrderFragment.kt
+++ app/src/main/java/com/group4/gostudy/presentation/detail/material/dialog/DialogOrderFragment.kt
@@ -5,12 +5,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.group4.gostudy.databinding.FragmentDialogOrderBinding
+import com.group4.gostudy.presentation.home.HomeViewModel
 import com.group4.gostudy.presentation.payment.PaymentActivity
+import com.group4.gostudy.utils.ApiException
+import com.group4.gostudy.utils.proceedWhen
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class DialogOrderFragment : Fragment() {
     private lateinit var binding: FragmentDialogOrderBinding
+    private val dialogOrderViewModel: DialogOrderViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,9 +27,60 @@ class DialogOrderFragment : Fragment() {
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setClickListener()
+        observeData()
+    }
+
+    private fun observeData() {
+        dialogOrderViewModel.getCourse()
+        dialogOrderViewModel.courses.observe(
+            viewLifecycleOwner
+        ) {
+            it.proceedWhen(
+                doOnLoading = {
+                    binding.layoutStateOrder.root.isVisible =
+                        true
+                    binding.layoutStateOrder.animLoading.isVisible =
+                        true
+                    binding.layoutStateOrder.llAnimError.isVisible =
+                        false
+                    binding.layoutContent.root.isVisible =
+                        false
+                    binding.btnOrder.isVisible = false
+                },
+                doOnSuccess = {
+                    binding.layoutStateOrder.root.isVisible =
+                        true
+                    binding.layoutStateOrder.animLoading.isVisible =
+                        false
+                    binding.layoutStateOrder.llAnimError.isVisible =
+                        false
+                    binding.layoutContent.root.isVisible =
+                        true
+                    binding.btnOrder.isVisible = true
+                },
+                doOnError = {
+                    binding.layoutStateOrder.root.isVisible =
+                        true
+                    binding.layoutStateOrder.animLoading.isVisible =
+                        false
+                    binding.layoutStateOrder.llAnimError.isVisible =
+                        true
+                    binding.layoutContent.root.isVisible =
+                        false
+                    binding.btnOrder.isVisible = false
+                    if (it.exception is ApiException) {
+                        binding.layoutStateOrder.tvError.isVisible =
+                            true
+                        binding.layoutStateOrder.tvError.text =
+                            it.exception.getParsedError()?.message
+                    }
+                }
+            )
+        }
     }
 
     private fun setClickListener() {
