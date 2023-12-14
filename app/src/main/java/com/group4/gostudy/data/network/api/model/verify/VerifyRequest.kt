4ac61c95e2f6b92cd6214ed76c2e9ae3aba2def7
rizkyyilman <nuugrahaaa@gmail.com>
2023-12-15 04:26:28 +0700

    adding verify request

---
+++ app/src/main/java/com/group4/gostudy/data/network/api/model/verify/VerifyRequest.kt
@@ -0,0 +1,12 @@
+package com.group4.gostudy.data.network.api.model.verify
+
+import androidx.annotation.Keep
+import com.google.gson.annotations.SerializedName
+
+@Keep
+data class VerifyRequest(
+    @SerializedName("otp")
+    val otp: String?,
+    @SerializedName("token")
+    val token: String?
+)
