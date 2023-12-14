b847eff4230f488a358a383099e488c0a2922f67
rizkyyilman <nuugrahaaa@gmail.com>
2023-12-15 04:26:28 +0700

    adding verify request

---
+++ app/src/main/java/com/group4/gostudy/data/network/api/model/verify/VerifyResponse.kt
@@ -0,0 +1,9 @@
+package com.group4.gostudy.data.network.api.model.verify
+
+import androidx.annotation.Keep
+import com.group4.gostudy.data.network.api.model.common.BaseResponse
+
+@Keep
+data class VerifyResponse(
+    val response: BaseResponse
+)
