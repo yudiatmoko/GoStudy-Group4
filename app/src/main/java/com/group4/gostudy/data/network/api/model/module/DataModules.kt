3cad912213dbf3492f9dbe2f7539c17eabe930de
Ninda Ack <ninda2752@gmail.com>
2023-12-17 18:53:13 +0700

    edit model API

--- app/src/main/java/com/group4/gostudy/data/network/api/model/module/DataModules.kt
+++ app/src/main/java/com/group4/gostudy/data/network/api/model/module/DataModules.kt
@@ -1,6 +1,5 @@
 package com.group4.gostudy.data.network.api.model.module
 
-import com.ackerman.popup.model.modelapi.module.ModuleItemResponse
 import com.google.gson.annotations.SerializedName
 
 data class DataModules(
