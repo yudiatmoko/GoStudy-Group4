cf4dc1ef62ff8d530c9c52afcbb1e5da18204ed0
Ninda Ack <ninda2752@gmail.com>
2023-12-26 10:47:55 +0700

    add navigation to payment

--- app/src/main/java/com/group4/gostudy/presentation/detail/material/dialog/DialogOrderViewModel.kt
+++ app/src/main/java/com/group4/gostudy/presentation/detail/material/dialog/DialogOrderViewModel.kt
@@ -1,16 +1,25 @@
 package com.group4.gostudy.presentation.detail.material.dialog
 
+import android.os.Bundle
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.group4.gostudy.data.repository.CourseRepository
 import com.group4.gostudy.model.Course
+import com.group4.gostudy.presentation.detail.DetailCourseActivity
 import com.group4.gostudy.utils.ResultWrapper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class DialogOrderViewModel(private val courseRepo: CourseRepository) : ViewModel() {
+class DialogOrderViewModel(
+    private val courseRepo: CourseRepository,
+    private val extras: Bundle?
+) : ViewModel() {
+    val course = extras?.getParcelable<Course>(DetailCourseActivity.EXTRA_PRODUCT)
+
+    var idCourse: Int? = course?.id
+
     private val _courses =
         MutableLiveData<ResultWrapper<Course>>()
 
