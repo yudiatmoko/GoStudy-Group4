8660c5593fece1eb45b8d0bd783528fa4fab5c08
xdiffan <irfanda4567@gmail.com>
2023-12-27 16:33:28 +0700

    implement api view-course,query search,type, util  convert second to minutes

---
+++ app/src/main/java/com/group4/gostudy/data/network/api/model/usercourseid/Module.kt
@@ -0,0 +1,52 @@
+package com.group4.gostudy.data.network.api.model.usercourseid
+
+import androidx.annotation.Keep
+import com.google.gson.annotations.SerializedName
+import com.group4.gostudy.model.Module
+
+@Keep
+data class UserModule(
+    @SerializedName("chapterId")
+    val chapterId: Int?,
+    @SerializedName("createdAt")
+    val createdAt: String?,
+    @SerializedName("createdBy")
+    val createdBy: Int?,
+    @SerializedName("description")
+    val description: String?,
+    @SerializedName("duration")
+    val duration: Int?,
+    @SerializedName("id")
+    val id: Int?,
+    @SerializedName("isUnlocked")
+    val isUnlocked: Boolean?,
+    @SerializedName("name")
+    val name: String?,
+    @SerializedName("noModule")
+    val noModule: Int?,
+    @SerializedName("updatedAt")
+    val updatedAt: String?,
+    @SerializedName("videoId")
+    val videoId: String?,
+    @SerializedName("videoUrl")
+    val videoUrl: String?
+)
+
+fun UserModule.toModule() = Module(
+    id = this.id ?: 0,
+    noModule = this.noModule ?: 0,
+    chapterId = this.chapterId ?: 0,
+    name = this.name.orEmpty(),
+    createdAt = this.createdAt.orEmpty(),
+    updatedAt = this.updatedAt.orEmpty(),
+    createdBy = this.createdBy ?: 0,
+    description = this.description.orEmpty(),
+    videoId = this.videoId.orEmpty(),
+    videoUrl = this.videoUrl.orEmpty(),
+    duration = this.duration ?: 0,
+    isUnlocked = this.isUnlocked
+)
+
+fun Collection<UserModule>.toModuleList() = this.map {
+    it.toModule()
+}
--- app/src/main/java/com/group4/gostudy/data/repository/DetailRepository.kt
+++ app/src/main/java/com/group4/gostudy/data/repository/DetailRepository.kt
@@ -1,13 +1,17 @@
 package com.group4.gostudy.data.repository
 
 import com.group4.gostudy.data.network.api.datasource.GoStudyApiDataSource
+import com.group4.gostudy.data.network.api.model.coursev2.toCourse
 // import com.group4.gostudy.data.network.api.model.course.toCourseList
 import com.group4.gostudy.data.network.api.model.detail.toCourseList
+import com.group4.gostudy.model.Course
 import com.group4.gostudy.model.DetailCourse
 import com.group4.gostudy.utils.ResultWrapper
 import com.group4.gostudy.utils.proceedFlow
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
 
 interface DetailRepository {
     suspend fun getCourseID(
@@ -15,6 +19,7 @@ interface DetailRepository {
         module: String?,
         chapter: String?
     ): Flow<ResultWrapper<List<DetailCourse>>>
+    suspend fun getUserCourseById(id: Int): Flow<ResultWrapper<Course>>
 }
 
 class DetailRepositoryImpl(
@@ -40,6 +45,17 @@ class DetailRepositoryImpl(
                 ?: emptyList()
         }.catch {
             emit(ResultWrapper.Error(Exception(it)))
+        }
+    }
+
+    override suspend fun getUserCourseById(id: Int): Flow<ResultWrapper<Course>> {
+        return proceedFlow {
+            apiDataSource.getCourseById(id).data.course.toCourse()
+        }.catch {
+            emit(ResultWrapper.Error(Exception(it)))
+        }.onStart {
+            emit(ResultWrapper.Loading())
+            delay(2000)
         }
     }
 }
