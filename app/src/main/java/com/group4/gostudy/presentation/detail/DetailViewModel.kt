b908e548c6957d6a2f20282436038ea86ca00041
xdiffan <irfanda4567@gmail.com>
2023-12-27 23:53:10 +0700

    navigate to detail

--- app/src/main/java/com/group4/gostudy/presentation/detail/DetailViewModel.kt
+++ app/src/main/java/com/group4/gostudy/presentation/detail/DetailViewModel.kt
@@ -7,7 +7,7 @@ import com.group4.gostudy.model.Course
 
 class DetailViewModel(
     private val extras: Bundle?,
-    private val detailRepository: DetailRepository
+    private val userCourseRepository: UserCourseRepository
 
 ) : ViewModel() {
     val course = extras?.getParcelable<Course>(DetailCourseActivity.EXTRA_PRODUCT)
@@ -16,16 +16,24 @@ class DetailViewModel(
 
     val desc: String? = course?.description
 
-  /*  private val _detail = MutableLiveData<ResultWrapper<List<PopularCourse>>>()
+    private val _detail = MutableLiveData<ResultWrapper<Course?>>()
 
-    val details: LiveData<ResultWrapper<List<DetailCourse>>>
+    val details: LiveData<ResultWrapper<Course?>>
         get() = _detail
 
-    fun getDetails(category: String? = null, module: String? = null, chapter: String? = null) {
+    fun getDetail() {
         viewModelScope.launch(Dispatchers.IO) {
-            detailRepository.getCourseID(category, module,chapter).collect {
+            userCourseRepository.getUserCourseById(idCourse ?: 0).collect {
                 _detail.postValue(it)
             }
         }
-    }*/
+    }
+
+    /*    fun getDetails(category: String? = null, module: String? = null, chapter: String? = null) {
+            viewModelScope.launch(Dispatchers.IO) {
+                detailRepository.getCourseID(category, module,chapter).collect {
+                    _detail.postValue(it)
+                }
+            }
+        }*/
 }
